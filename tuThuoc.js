bf4a1b72504f3b01964881cb18ed14286a63cafd
maucat1up <hannnn.0801@gmail.com>
2022-05-18 10:49:11 +0700

    first commit

---
+++ tuThuoc.js
@@ -0,0 +1,113 @@
+var a =`<div id="boxTuThuoc">
+<div class="header">
+        <img class=" header__img" src="./images_tuthuoc/Vector (6).png" alt="">
+            <span class="header__text"> Tủ Thuốc Gia Đình </span>
+        <img class="header__logo" src="./images_tuthuoc/image 16.png" alt="">
+</div>
+
+<div class="slider">
+    <img class="slider__above" src="./images_tuthuoc/Group 5.png" alt="">
+    <img class="slider__back" src="./images_tuthuoc/Rectangle 5.png" alt="">
+
+    <div class="slider__tab">
+        <img src="./images_tuthuoc/Ellipse 1.png" alt="">
+        <img src="./images_tuthuoc/Ellipse 2.png" alt="">
+        <img src="./images_tuthuoc/Ellipse 2.png" alt="">
+    </div>
+</div>
+<div class="footer">
+            <p class=" footer__text">
+                <span style="font-weight:500;"> Khuyến cáo:</span> Sản phẩm này không phải là thuốc và không có tác dụng thay thế thuốc chữa bệnh 
+            </p>
+</div>
+
+<div class="content">
+    <div class="content__main">
+        <div class="content__main--right">
+            <div class="main">
+                <div class="main__image">
+                    <img src="./images_tuthuoc/image 5.png" alt="">
+                </div>
+                <div class="main__tittle">
+                    <h4 class ="main__tittle--up"> TPBVSK Kim Não Khang </h4>
+                    <p class="main__tittle--down"> Không còn đau đầu, chóng mặt, mất ngủ. Chiết xuất từ thảo dược 100% thiên nhiên </p>
+                </div>
+                <div class="main__button">
+                    <button class="main__button--button"> TÌM HIỂU NGAY </button>
+                </div>
+            </div>
+        </div>
+        <div class="content__main--left">
+            <div class="main">
+                <div class="main__image">
+                    <img src="./images_tuthuoc/image 5.png" alt="">
+                </div>
+                <div class="main__tittle">
+                    <h4 class ="main__tittle--up"> TPBVSK Kim Não Khang </h4>
+                    <p class="main__tittle--down"> Không còn đau đầu, chóng mặt, mất ngủ. Chiết xuất từ thảo dược 100% thiên nhiên </p>
+                </div>
+                <div class="main__button">
+                    <button class="main__button--button"> TÌM HIỂU NGAY </button>
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+</div>`
+var scss =`<div id="boxTuThuoc">
+<div class="header">
+        <img class=" header__img" src="./images_tuthuoc/Vector (6).png" alt="">
+            <span class="header__text"> Tủ Thuốc Gia Đình </span>
+        <img class="header__logo" src="./images_tuthuoc/image 16.png" alt="">
+</div>
+
+<div class="slider">
+    <img class="slider__above" src="./images_tuthuoc/Group 5.png" alt="">
+    <img class="slider__back" src="./images_tuthuoc/Rectangle 5.png" alt="">
+
+    <div class="slider__tab">
+        <img src="./images_tuthuoc/Ellipse 1.png" alt="">
+        <img src="./images_tuthuoc/Ellipse 2.png" alt="">
+        <img src="./images_tuthuoc/Ellipse 2.png" alt="">
+    </div>
+</div>
+<div class="footer">
+            <p class=" footer__text">
+                <span style="font-weight:500;"> Khuyến cáo:</span> Sản phẩm này không phải là thuốc và không có tác dụng thay thế thuốc chữa bệnh 
+            </p>
+</div>
+
+<div class="content">
+    <div class="content__main">
+        <div class="content__main--right">
+            <div class="main">
+                <div class="main__image">
+                    <img src="./images_tuthuoc/image 5.png" alt="">
+                </div>
+                <div class="main__tittle">
+                    <h4 class ="main__tittle--up"> TPBVSK Kim Não Khang </h4>
+                    <p class="main__tittle--down"> Không còn đau đầu, chóng mặt, mất ngủ. Chiết xuất từ thảo dược 100% thiên nhiên </p>
+                </div>
+                <div class="main__button">
+                    <button class="main__button--button"> TÌM HIỂU NGAY </button>
+                </div>
+            </div>
+        </div>
+        <div class="content__main--left">
+            <div class="main">
+                <div class="main__image">
+                    <img src="./images_tuthuoc/image 5.png" alt="">
+                </div>
+                <div class="main__tittle">
+                    <h4 class ="main__tittle--up"> TPBVSK Kim Não Khang </h4>
+                    <p class="main__tittle--down"> Không còn đau đầu, chóng mặt, mất ngủ. Chiết xuất từ thảo dược 100% thiên nhiên </p>
+                </div>
+                <div class="main__button">
+                    <button class="main__button--button"> TÌM HIỂU NGAY </button>
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+</div>`
+document.getElementById('boxTuThuoc').innerHTML= `<style>${scss}</style> ${a}`
